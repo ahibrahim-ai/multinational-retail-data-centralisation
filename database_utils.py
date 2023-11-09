6ad28073ed42c608461877844990bbcfd9c54beb
Sous Abraham <theaateam24@gmail.com>
2023-11-09 10:40:22 +0000

    Adds db connector

--- database_utils.py
+++ database_utils.py
@@ -1,3 +1,41 @@
+import yaml
+import sqlalchemy 
+from sqlalchemy import create_engine
+
 class DatabaseConnector:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, credentials_file_path) -> None:
+        self.credentials_file_path = credentials_file_path
+        self.credentials = self.read_db_creds()
+        self.engine = self.init_db_engine()
+
+    def read_db_creds(self):
+        try:
+            with open(self.credentials_file_path, 'r') as file:
+                credentials = yaml.safe_load(file)
+                return credentials
+        except FileNotFoundError:
+            print(f"Error: File not found at {self.credentials_file_path}")
+        except yaml.YAMLError as e:
+            print(f"Error reading YAML file: {e}")
+
+    def init_db_engine(self):
+        try:
+            db_url = (
+                f"postgresql://{self.credentials['RDS_USER']}:{self.credentials['RDS_PASSWORD']}@"
+                f"{self.credentials['RDS_HOST']}:{self.credentials['RDS_PORT']}/{self.credentials['RDS_DATABASE']}"
+            )
+            engine = create_engine(db_url)
+            return engine
+        except (KeyError, TypeError) as e:
+            print(f"Error: Missing key in credentials - {e}")
+        except Exception as e:
+            print(f"Error initializing database engine: {e}")
+
+# Example usage:
+file_path = '/Users/sousabraham/project2/multinational-retail-data-centralisation/db_creds.yaml'
+db_connector = DatabaseConnector(file_path)
+
+if hasattr(db_connector, 'credentials') and hasattr(db_connector, 'engine'):
+    print("Database engine initialized successfully.")
+else:
+    print("Failed to initialize database engine.")
